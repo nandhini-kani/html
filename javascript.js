d7f8bd86d98b6fad2ef08db4622eda63db8cf0f3
nandhini-kani <nandhukarthi2004@gmail.com>
2025-07-28 16:40:23 +0530

    java script

---
+++ javascript.js
@@ -0,0 +1,27 @@
+var a=9;
+var b=9;
+a=12;
+console.log(a+b);
+ 
+var s=3;
+s=8;
+console.log( "var :" ,typeof s);
+
+let n='nandhini';
+console.log("let :", typeof n );
+
+const num=90;
+
+console.log("const :",typeof num );
+
+
+var data={name:'nandhini',
+    age:21,
+    date:"20.092004",
+}
+console.log("date", data)
+
+
+var s=[4,5,7,8,9];
+console.log("Array :",s);
+
